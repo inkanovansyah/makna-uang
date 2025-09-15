b07f7b85f921b031c11e49d34a751548a071fb11
inkanovansyah <inkarock18@gmail.com>
2025-09-15 10:08:30 +0700

    nambah analitik

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,32 @@
+// const siteMetadata = require("./src/utils/siteMetaData");
+
+
+// module.exports = {
+//     siteUrl: siteMetadata.siteUrl,
+//     generateRobotsTxt: true,
+//   }
 const siteMetadata = require("./src/utils/siteMetaData");
 
-
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: siteMetadata.siteUrl,
-    generateRobotsTxt: true,
-  }
+  siteUrl: siteMetadata.siteUrl || "https://maknauang.com",
+  generateRobotsTxt: true, // generate robots.txt otomatis
+  changefreq: "daily", // default untuk semua halaman
+  priority: 0.7, // default priority
+  sitemapSize: 5000,
+  exclude: [
+    "/manifest.webmanifest",
+    "/apple-icon.png",
+    "/favicon.ico",
+    "/404",
+    "/500",
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/", // izinkan semua halaman frontend
+      },
+    ],
+  },
+};
